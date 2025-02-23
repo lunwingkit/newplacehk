b84b1536d628555c00f6dd1b5dde4f225740ffb7
Killian LUN Wing Kit <lunwingkit@gmail.com>
2025-02-23 17:09:01 +0800

    ok

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
             allowedForwardedHosts: ['localhost', 'https://solid-fishstick-vgqjgjjv777fxpp.github.dev'],
             allowedOrigins: ['http://localhost', "https://solid-fishstick-vgqjgjjv777fxpp.github.dev", 'localhost:3000']
         },
+        runtime: "nodejs", // Force Node.js runtime
     },
     images: {
         domains: ['miquan-play.oss-cn-shenzhen.aliyuncs.com'], // Add the external domain here
